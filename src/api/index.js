1e7fe6b7373c1ed15a0c43191eb8fb950d6ab147
Mridul00334 <mridul@swadec.com>
2024-12-06 20:59:03 +0530

    first commit

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ let local_url="http://localhost:9000"
 
 export const getList = async () => {
     try {
-      const response = await axios.get(`${local_url}/get-list`);
+      const response = await axios.get(`${base_url}/get-list`);
       
       const result =  response.data;
       return result;  // Return the data to be used by the component
